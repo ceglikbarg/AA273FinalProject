effbb885e209cd05bbde81576deb6a1269390ba2
Max Newport <newpomax@gmail.com>
2021-06-01 23:37:53 -0700

    Updated system to estimate volume (true fed-batch) using provided volume measurements. Handles EKF, iEKF, UKF, and PF, and control is fairly functional so long as estimate does not diverge.

--- plotsim.m
+++ plotsim.m
@@ -1,21 +1,26 @@
 function plotsim(constants, tout, x, mu, cov, y, u, targ, targ_err)
+    c1 = [0.9290 0.6940 0.1250];
+    c2 = [0.4940 0.1840 0.5560];
+    filters = {'EKF','iEKF','UKF','PF'};
+    filter = filters{constants.filter};
     %% SIM OUTPUT
     figure('Name','Concentrations'); hold on;
+    title('Substrate and Cell Concentrations');
     ylabel('Concentration, g/L'); xlabel('time, sec');
-    cov_patch(1,'Substrate 95% CI');
-    cov_patch(2,'Cell 95% CI');
+    cov_patch(1,'Substrate 95% CI',c1);
+    cov_patch(2,'Cell 95% CI',c2);
     plot(tout, x(:,1),'DisplayName','Substrate Truth');
     plot(tout, x(:,2),'DisplayName','Cell Truth');
-    plot(tout, mu(:,1),'DisplayName','Substrate EKF');
-    plot(tout, mu(:,2),'--','DisplayName','Cell EKF');
+    plot(tout, mu(:,1),'--','Color',c1,'DisplayName',['Substrate ' filter]);
+    plot(tout, mu(:,2),'--','Color',c2,'DisplayName',['Cell ' filter]);
     legend();
 
     figure('Name', 'O2 Consumption'); hold on;
     title('Specific O_2 Consumption Rate ~ Q_{O2}');
     ylabel('Specific Consumption, mol O_2 per sec per hour'); xlabel('time, sec');
-    cov_patch(3,'Q_{O2} 95% CI');
+    cov_patch(3,'Q_{O2} 95% CI',c1);
     plot(tout, x(:,3),'DisplayName','Q_{O2} Truth');
-    plot(tout, mu(:,3),'DisplayName','Q_{O2} EKF');
+    plot(tout, mu(:,3),'--','Color',c1,'DisplayName',['Q_{O2} ' filter]);
     legend();
     
     % Calculate OR mean and cov
@@ -26,20 +31,28 @@ function plotsim(constants, tout, x, mu, cov, y, u, targ, targ_err)
     title('Oxygen Transfer Rate = Oxygen Uptake Rate');
     ylabel('O_R, moles O2/sec'); xlabel('time, sec');
     ORspread = [mu_OR+2*sqrt(cov_OR); flip(mu_OR-2*sqrt(cov_OR))];
-    patch([tout; flip(tout)], ORspread , [0.4 0.4 0.4], 'FaceAlpha',0.1,'EdgeColor','none','DisplayName','O_R 95% CI');
+    patch([tout; flip(tout)], ORspread , [0.4 0.4 0.4], 'FaceColor',c1,'FaceAlpha',0.2,'EdgeColor','none','DisplayName','O_R 95% CI');
     plot(tout, x(:,2).*x(:,3),'DisplayName','O_R Truth');
-    plot(tout, mu_OR,'DisplayName','O_R EKF');
+    plot(tout, mu_OR,'--','Color', c1,'DisplayName',['O_R ' filter]);
     legend();
 
     figure('Name', 'Temps'); hold on;
     title('System Temperatures');
     ylabel('Temperature, deg F'); xlabel('time, sec');
-    cov_patch(4,'T_C 95% CI');
-    cov_patch(5,'T_\infty 95% CI');
+    cov_patch(4,'T_C 95% CI',c1);
+    cov_patch(5,'T_\infty 95% CI',c2);
     plot(tout,convtemp(x(:,4),'K','F'),'DisplayName','T_C Truth');
     plot(tout,convtemp(x(:,5),'K','F'),'DisplayName','T_\infty Truth');
-    plot(tout,convtemp(mu(:,4),'K','F'),'DisplayName','T_C EKF');
-    plot(tout,convtemp(mu(:,5),'K','F'),'DisplayName','T_\infty EKF');
+    plot(tout,convtemp(mu(:,4),'K','F'),'--','Color',c1,'DisplayName',['T_C ' filter]);
+    plot(tout,convtemp(mu(:,5),'K','F'),'--','Color',c2,'DisplayName',['T_\infty ' filter]);
+    legend();
+    
+    figure('Name', 'Volume'); hold on;
+    title('Volume of Culture');
+    ylabel('V, L'); xlabel('time, sec');
+%     cov_patch(6,'V 95% CI',c1);
+    plot(tout, x(:,6),'DisplayName','V Truth');
+    plot(tout, mu(:,6),'--','Color',c1,'DisplayName',['V ' filter]);
     legend();
 
     %% SIM CONTROL
@@ -55,11 +68,11 @@ function plotsim(constants, tout, x, mu, cov, y, u, targ, targ_err)
     
     %% SIM TARGETS
     
-    function cov_patch(idx, descrip)
+    function cov_patch(idx, descrip, c)
        myspread = [mu(:,idx)+2*sqrt(squeeze(cov(idx,idx,:))); flip(mu(:,idx)-2*sqrt(squeeze(cov(idx,idx,:))))];
        if idx > 3
           myspread = convtemp(myspread,'K','F'); 
        end
-       patch([tout; flip(tout)], myspread , [0.4 0.4 0.4], 'FaceAlpha',0.1,'EdgeColor','none','DisplayName',descrip);
+       patch([tout; flip(tout)], myspread , [0.4 0.4 0.4], 'FaceColor', c,'FaceAlpha',0.1,'EdgeColor','none','DisplayName',descrip);
     end
 end
--- sim_constants.m
+++ sim_constants.m
@@ -4,7 +4,8 @@
 % Simulation constants
 constants.dt = 0.1;
 constants.tend= 3600;
-constants.filter = FilterTypes.iEKF;
+constants.filter = FilterTypes.UKF;
+constants.num_particles = 50; % number of particles (only influences for PF)
 
 % System constants
 constants.mu_max = 0.806/3600; % max growth rate, 1/sec
@@ -12,8 +13,8 @@ constants.Ki = 87.4; % inhibition coefficient, g/L
 constants.Ks = 0.68; % saturation coefficient, g/L
 constants.Yxs = 6*11.8/180; % yield coeff, g/g (11.8 g per mole Carbon * (6/180 moles Carbon per g glucose)
 constants.m = (0.103/1000/3600)*180; % maintenance coeff, (mol/g/s)*g/mol = g/g/s
-constants.V = 2; % volume, L
-constants.C_heat = 4.314e3*constants.V; % specific heat, J/K
+constants.density = 1000; % density of fluid, g/L
+constants.C_heat = 4.314*constants.density; % volume-specific heat capacity, J/K/L
 constants.N_power = 2*10^-10;
 constants.Kq = 470000; % bacteria heat coefficient, J/(mol O2)
 constants.Pp = 20; % peltier coefficient, W/A
@@ -25,25 +26,27 @@ constants.S0 = 12; % concentration of substrate in feed, g/L
 constants.pr = 1.2e5; % pressure in reactor, Pa
 
 % Initial Conditions
-S = 4; % g/l, initial substrate concentration
-C = 2; % g/l, initial cell concentration
+S = 2; % g/l, initial substrate concentration
+C = 3; % g/l, initial cell concentration
 QO2 = 0.0198/3600; % specific O2 consumption rate for e. coli and glusoce (in mol/O2 per sec per gram)
 Tc = 273.15+35; % culture temp initially, degK
 Tinf = 290; % environment temp
-constants.x0 = [S; C; QO2; Tc; Tinf];
+V = 2; %initial volume, L
+constants.x0 = [S; C; QO2; Tc; Tinf;V];
 
 % EKF Constants
 constants.Q = 1E-3*eye(length(constants.x0));
 constants.Q(1:2,1:2) = 1E-6*constants.dt*eye(2);
 constants.Q(3,3) = 1E-15*constants.dt;
-constants.R = eye(4);
+constants.R = eye(5);
 constants.R(1,1) = (0.001/2)^2; % mass fraction sensor has 0.1% percent by vol error
 constants.R(2,2) = (0.002/2)^2; % D sensor has 0.2% by vol error
 constants.R(3,3) = (0.2/2)^2; % internal thermistor is +/- 0.2deg
 constants.R(4,4) = (1/2)^2; % external thermistor is +/- 1deg
+constants.R(5,5) = (0.05*V/2)^2; % volume measurement is +/- 0.05*initial volume
 
-constants.mu0 = constants.x0 + [-0.0004; -0.0001; -0.0000; 0.0059; -0.0048];
-constants.cov0 = 0.1*eye(length(constants.mu0));
+constants.mu0 = constants.x0 + [-0.0004; -0.0001; -0.0000; 0.0059; -0.0048; 0.01];
+constants.cov0 = 1E-3*eye(length(constants.mu0));
 
 % Targetting
 constants.w = 450; % agitator speed, RPM
